0d8e5425fd6be46ec7662eee30f4c77449aeb4ea
JOY <joybosu@gmail.com>
2022-03-03 21:11:39 +0530

    upload Updated Program foile

--- Program.cs
+++ Program.cs
@@ -7,36 +7,36 @@ namespace InventoryDataManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Hello inventory management system");
-            //string path = "F:/InventoryDataManagement/Rice.json";
+            string path = "F:/InventoryDataManagement/Rice.json";
 
-            //FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
-            //Rice data = fetchJsonForRice.Read(path);
+            FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+            Rice data = fetchJsonForRice.Read(path);
 
-            //for (int i = 0; i < data.typesofRice.Count; i++)
-            //{
-            //    Console.WriteLine(data.typesofRice[i].name);
-            //    Console.WriteLine(data.typesofRice[i].weight);
-            //    Console.WriteLine(data.typesofRice[i].Price);
-            //    Console.WriteLine("---------*--------**--------");
-            //}
+            for (int i = 0; i < data.typesofRice.Count; i++)
+            {
+                Console.WriteLine(data.typesofRice[i].name);
+                Console.WriteLine(data.typesofRice[i].weight);
+                Console.WriteLine(data.typesofRice[i].Price);
+                Console.WriteLine("---------*--------**--------");
+            }
 
-            //for (int i = 0; i < data.typesofPulse.Count; i++)
-            //{
-            //    Console.WriteLine(data.typesofPulse[i].name);
-            //    Console.WriteLine(data.typesofPulse[i].weight);
-            //    Console.WriteLine(data.typesofPulse[i].Price);
-            //    Console.WriteLine("---------*--------**--------");
+            for (int i = 0; i < data.typesofPulse.Count; i++)
+            {
+                Console.WriteLine(data.typesofPulse[i].name);
+                Console.WriteLine(data.typesofPulse[i].weight);
+                Console.WriteLine(data.typesofPulse[i].Price);
+                Console.WriteLine("---------*--------**--------");
 
-            //}
+            }
 
-            //for (int i = 0; i < data.typesofWheat.Count; i++)
-            //{
-            //    Console.WriteLine(data.typesofWheat[i].name);
-            //    Console.WriteLine(data.typesofWheat[i].weight);
-            //    Console.WriteLine(data.typesofWheat[i].Price);
-            //    Console.WriteLine("---------*--------**--------");
+            for (int i = 0; i < data.typesofWheat.Count; i++)
+            {
+                Console.WriteLine(data.typesofWheat[i].name);
+                Console.WriteLine(data.typesofWheat[i].weight);
+                Console.WriteLine(data.typesofWheat[i].Price);
+                Console.WriteLine("---------*--------**--------");
 
-            //}
+            }
 
             //Console.WriteLine(data.name);
             //Console.WriteLine(data.);
