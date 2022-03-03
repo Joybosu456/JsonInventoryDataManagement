10dad3f8ebc481894eddfd3a03956168d65ed59c
JOY <joybosu@gmail.com>
2022-03-03 21:08:36 +0530

    inventory Welclome Program file upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace InventoryDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello inventory management system");
+            //string path = "F:/InventoryDataManagement/Rice.json";
+
+            //FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+            //Rice data = fetchJsonForRice.Read(path);
+
+            //for (int i = 0; i < data.typesofRice.Count; i++)
+            //{
+            //    Console.WriteLine(data.typesofRice[i].name);
+            //    Console.WriteLine(data.typesofRice[i].weight);
+            //    Console.WriteLine(data.typesofRice[i].Price);
+            //    Console.WriteLine("---------*--------**--------");
+            //}
+
+            //for (int i = 0; i < data.typesofPulse.Count; i++)
+            //{
+            //    Console.WriteLine(data.typesofPulse[i].name);
+            //    Console.WriteLine(data.typesofPulse[i].weight);
+            //    Console.WriteLine(data.typesofPulse[i].Price);
+            //    Console.WriteLine("---------*--------**--------");
+
+            //}
+
+            //for (int i = 0; i < data.typesofWheat.Count; i++)
+            //{
+            //    Console.WriteLine(data.typesofWheat[i].name);
+            //    Console.WriteLine(data.typesofWheat[i].weight);
+            //    Console.WriteLine(data.typesofWheat[i].Price);
+            //    Console.WriteLine("---------*--------**--------");
+
+            //}
+
+            //Console.WriteLine(data.name);
+            //Console.WriteLine(data.);
+
+
+
+        }
+    }
+}
